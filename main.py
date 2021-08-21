bd8323fb34301b3b086efe87286a5531e387581b
Adarsh <adarsh08kumar@gmail.com>
2021-08-22 04:18:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from flask import Flask, jsonify, request, json
-
-#Characters which might cause sql injection
-injection = ['\'','1=1','""=""']
+from flask import Flask, jsonify, request, json, render_template
+from flask_mysqldb import MySQL
+import yaml
 
 app = Flask(__name__)
 
-@app.route("/v1/sanitized/input/",methods=['POST'])
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+mysql = MySQL(app)
+
+#Characters which might cause sql injection
+injection = ['\'','1=1','""=""','true','/*','*/','+','||','admin\'--',";","') or ('1'='1--","' or 1=1#","' or 1=1--","admin' #"]
+
+@app.route("/v1/sanitized/input/",methods=['GET','POST'])
 def index():    
     if request.method=='POST':
         data_string = request.get_data()
@@ -14,20 +23,30 @@ def index():
         except Exception as e:
             return str(e)
 
-        if len(data) :
+    
+        if len(data) : 
             if validate(data):
+                cur = mysql.connection.cursor()
+                for key in data:
+                    resultValue = cur.execute("SELECT name,password FROM users where name=%s and password=%s",(key,data[key]))
+                cur.close()
                 return jsonify({'result':'sanitized'})
             return jsonify({'result':'unsanitized'})
         else:
             return 'Bad Request', 400
-    
-    return "Method not allowed"
+    else:
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute("SELECT * FROM users")
+        if resultValue > 0:
+            userDetails = cur.fetchall()
+            return render_template('users.html',userDetails=userDetails)
     
 def validate(data):
+    
     for key in data:
         value = str(data[key])
         for char in injection:
-            if char in key.split() or char in value.split():
+            if str(char) in key or char in value:
                 return False
         return True
 
